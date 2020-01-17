05ff5ff2a2c731e447413d86eff88e68c7a3212e
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-17 20:28:49 -0300

    UPDATE FILE
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import api from './services/api';
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
-import './global.css';
-import './App.css';
-import './Sidebar.css';  
-import './Main.css';
+import "./global.css";
+import "./App.css";
+import "./Sidebar.css";
+import "./Main.css";
 
-import './components/DevItem';
-import DevItem from './components/DevItem';
-import DevForm from './components/DevForm';
-
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
 
 function App() {
   const [devs, setDevs] = useState([]);
 
   useEffect(() => {
-    async function loadDevs(){
-      const response = await api.get('/devs');
+    async function loadDevs() {
+      const response = await api.get("/devs");
 
       setDevs(response.data);
     }
@@ -25,9 +23,9 @@ function App() {
   }, []);
 
   async function handleAddDev(data) {
-    const response = await api.post('/devs', data);
+    const response = await api.post("/devs", data);
 
-    setDevs([...devs, response.data])
+    setDevs([...devs, response.data]);
   }
 
   return (
@@ -38,14 +36,14 @@ function App() {
       </aside>
 
       <main>
-      <ul>
-      {devs.map(dev => (
-        <DevItem key={dev._id} dev={dev} />
-      ))}
-      </ul>
+        <ul>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+        </ul>
       </main>
     </div>
-  )
+  );
 }
 
 export default App;
