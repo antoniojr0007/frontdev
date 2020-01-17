52381d384424d78347b22db1e22782695ea9ff83
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-17 20:20:46 -0300

    UPLOAD FILES
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

--- src/components/DevItem/index.js
+++ src/components/DevItem/index.js
@@ -1,39 +1,21 @@
-import React from "react";
-import Icon from "../Icon";
+import React from 'react';
 
-import "./styles.css";
+import './styles.css';
 
-function DevItem({ dev, onEdit, onDelete }) {
-  const [{ editMode, dev: oldDev }, setEditMode] = onEdit;
-
-  function editDev() {
-    setEditMode({
-      editMode: oldDev._id !== dev._id ? true : !editMode, // Se selecionou um Dev diferente, obrigatóriamente editMode deve ser true.
-      dev // Novo Dev
-    });
-  }
-
-  function deleteDev() {
-    onDelete(dev.github);
-  }
-
-  return (
-    <li className="dev-item">
-      <header>
-        <img src={dev.avatar_url} alt={dev.name} />
-        <div className="user-info">
-          <strong>{dev.name}</strong>
-          <span>{dev.techs.join(", ")}</span>
-        </div>
-        <div className="icons">
-          <Icon onClick={editDev} type="pen" />
-          <Icon onClick={deleteDev} type="trash" />
-        </div>
-      </header>
-      <p>{dev.bio}</p>
-      <a href={`https://github.com/${dev.github}`}>Acessar perfil no GitHub</a>
-    </li>
-  );
+function DevItem({ dev }) {
+    return (
+        <li className="dev-item">
+          <header>
+            <img alt={dev.name} src={dev.avatar_url}></img>
+            <div className="user-info">
+              <strong>{dev.name}</strong>
+              <span>{dev.techs.join(', ')}</span>
+            </div>
+          </header>
+          <p>{dev.bio}</p>
+          <a href={`https://github.com/${dev.github_username}`}>Acessar Github</a>
+        </li>
+    );
 }
 
 export default DevItem;
